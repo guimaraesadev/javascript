9d729d85f3aeeec90a68b588736812d7b19c9a89
Sabrina S. Guimarães <guimaraesadev@gmail.com>
2022-05-02 20:46:25 -0300

    Summary (required)

---
+++ JSThemes/JS Objetos JSON/index.js
@@ -0,0 +1,30 @@
+// MarcoBruno => https://www.youtube.com/watch?v=oCY5YEEjlwE
+
+
+// {JSON => JavaScript Object Notation
+// Objeto de anotação no JavaScript
+
+// representando variáveis porém elas não estão associadas;
+const title = "Harry Potter"
+const url = 'http://caminho.com/imagem'
+
+
+// montando um JSON:
+
+const movie = {
+    title: "Harry Potter", // propriedade/atributo do objeto 
+    url: "http://caminho.com/imagem", // preciso utilizar VÍRGULA na quebra de linha 
+};
+
+// Como eu faço para acessar algum atributo dentro do objeto JSON:
+
+movie.title
+movie.url 
+
+// o ponto mostra que você está buscando algo dentro de outra coisa. 
+
+
+
+
+
+
