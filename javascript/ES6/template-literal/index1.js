2ea5b3c0ffd998f8863efbe65ff9df56a8a6da59
Sabrina S. Guimarães <guimaraesadev@gmail.com>
2022-06-12 21:13:37 -0300

    template literal/template string

---
+++ javascript/ES6/template-literal/index1.js
@@ -0,0 +1,25 @@
+
+const ireland = {
+    live: 'Dublin',
+    love: 'Galway'
+};
+
+const text = 'Eu moro em ' + ireland.live + ' e sou apaixonado por ' + ireland.love+'!';
+
+const newText = `Eu moro em ${ireland.live} e sou apaixonado por ${ireland.love}!`;
+
+const fruits = 'bannana' 
+            + `\n`
+            + 'orange'
+            + `\n`
+            + 'apple';
+
+const newFruits = 
+`banana
+orange
+apple`;
+
+console.log(newText)
+console.log(text)
+console.log(newFruits)
+
